4e1a7ae8c750cfc34ab4b61657c33bc6744af6c4
mukutchowdhury <70929023+mukutchowdhury@users.noreply.github.com>
2024-08-26 19:43:20 -0400

    Template

---
+++ next.config.mjs
@@ -0,0 +1,27 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            svgoConfig: {
+              plugins: [
+                {
+                  name: 'removeViewBox',
+                  active: false,
+                },
+              ],
+            },
+          },
+        },
+      ],
+    })
+    return config
+  },
+}
+
+export default nextConfig
