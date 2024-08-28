52be34050c2cfcf1e2777a025883b979b4cb2788
synakhanna <sk9921@nyu.edu>
2024-08-27 20:17:19 -0700

    name change

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -6,8 +6,8 @@ const Layout = ({ children }) => {
     return (
         <>
             <Head>
-                <title>Mailgo</title>
-                <meta name='description' content="Gain control of your business's growth with Mailgo's comprehensive marketing, automation, and email marketing platform." />
+                <title>CollegeGenie</title>
+                <meta name='description' content="CollegeGenie is an AI-powered platform that helps you find the right colleges, streamline applications, and secure your future with personalized support every step of the way." />
                 <meta name='viewport' content='width=device-width, initial-scale=1' />
                 <link rel='icon' href='/favicon.ico' />
             </Head>
