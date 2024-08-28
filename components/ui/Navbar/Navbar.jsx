29bf4495b9b6633492c8e4fe524f24dfa6c06109
mukutchowdhury <70929023+mukutchowdhury@users.noreply.github.com>
2024-08-28 18:13:51 -0400

    Changed Navbar to make it responsive

--- components/ui/Navbar/Navbar.jsx
+++ components/ui/Navbar/Navbar.jsx
@@ -1,8 +1,8 @@
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Link from 'next/link';
 import { useEffect, useRef, useState } from 'react';
 import NavHeader from '../NavHeader';
 import NavLink from '../NavLink';
-import {SignedIn, SignedOut, UserButton} from "@clerk/nextjs"
 
 const Navbar = () => {
     const [state, setState] = useState(false);
@@ -41,8 +41,8 @@ const Navbar = () => {
                 if (start === null) start = currentTime;
                 const timeElapsed = currentTime - start;
                 const progress = Math.min(timeElapsed / duration, 1);
-                const easeInOutQuad = progress < 0.5 
-                    ? 2 * progress * progress 
+                const easeInOutQuad = progress < 0.5
+                    ? 2 * progress * progress
                     : -1 + (4 - 2 * progress) * progress;
                 const run = startPosition + distance * easeInOutQuad;
                 window.scrollTo(0, run);
@@ -58,36 +58,36 @@ const Navbar = () => {
 
     return (
         <header>
-        <nav className={`pb-5 md:text-lg md:static md:block ${state ? "bg-gray-900 absolute z-20 top-0 inset-x-0 rounded-b-2xl shadow-xl md:bg-gray-900" : "hidden"}`}>
-            <div className="custom-screen items-center md:flex">
-                <NavHeader state={state} onClick={() => setState(!state)} />
-                <div className={`flex-1 items-center mt-8 text-gray-300 md:font-medium md:mt-0 md:flex ${state ? 'block' : 'hidden'} `}>
-                    <ul className="flex-1 justify-center items-center mt-5 space-y-6 md:flex md:space-x-8 md:space-y-0">
-                        {navigation.map((item, idx) => (
-                            <li key={idx} className="hover:text-gray-50 text-lg font-semibold">
-                                <Link href={item.href} onClick={(e) => handleSmoothScroll(e, item.href)}>
-                                    {item.name}
-                                </Link>
-                            </li>
-                        ))}
-                    </ul>
-                    <div className="gap-x-6 items-center justify-end pt-5 space-y-6 md:flex md:space-y-0 md:mt-0">
-                        <SignedOut>
-                            <NavLink href="/sign-in" className="flex items-center justify-center gap-x-1 text-lg text-white font-medium custom-btn-bg border border-gray-500 active:bg-gray-900 md:inline-flex">
-                                Sign In
-                            </NavLink>
-                            <NavLink href="/sign-up" className="flex items-center justify-center gap-x-1 text-lg text-white font-medium custom-btn-bg border border-gray-500 active:bg-gray-900 md:inline-flex">
-                                Sign Up
-                            </NavLink>
-                        </SignedOut>
-                        <SignedIn>
-                            <UserButton afterSignOutUrl="/" />
-                        </SignedIn>
+            <nav className={`pb-5 md:text-lg md:static md:block bg-gray-900`}>
+                <div className="custom-screen items-center md:flex">
+                    <NavHeader state={state} onClick={() => setState(!state)} />
+                    <div className={`flex-1 items-center mt-8 text-gray-300 md:font-medium md:mt-0 md:flex ${state ? 'block' : 'hidden'} md:block`}>
+                        <ul className="flex-1 justify-center items-center mt-5 space-y-6 md:flex md:space-x-8 md:space-y-0">
+                            {navigation.map((item, idx) => (
+                                <li key={idx} className="hover:text-gray-50 text-lg font-semibold">
+                                    <Link href={item.href} onClick={(e) => handleSmoothScroll(e, item.href)}>
+                                        {item.name}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                        <div className="gap-x-6 items-center justify-end pt-5 space-y-6 md:flex md:space-y-0 md:mt-0">
+                            <SignedOut>
+                                <NavLink href="/sign-in" className="flex items-center justify-center gap-x-1 text-lg text-white font-medium custom-btn-bg border border-gray-500 active:bg-gray-900 md:inline-flex">
+                                    Sign In
+                                </NavLink>
+                                <NavLink href="/sign-up" className="flex items-center justify-center gap-x-1 text-lg text-white font-medium custom-btn-bg border border-gray-500 active:bg-gray-900 md:inline-flex">
+                                    Sign Up
+                                </NavLink>
+                            </SignedOut>
+                            <SignedIn>
+                                <UserButton afterSignOutUrl="/" />
+                            </SignedIn>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </nav>
-    </header>
+            </nav>
+        </header>
     );
 };
 
