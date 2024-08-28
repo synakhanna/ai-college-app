37428999338a8990272963cf530301199f27a8e2
synakhanna <sk9921@nyu.edu>
2024-08-27 22:39:55 -0700

    updated favicon

--- components/ui/Brand/Brand.jsx
+++ components/ui/Brand/Brand.jsx
@@ -2,8 +2,8 @@ import Image from "next/image"
 
 const Brand = ({ ...props }) => (
     <Image
-        src="/mailgo.svg"
-        alt="Mailgo logo"
+        src="/logo.png"
+        alt="CollegeGenie Logo"
         {...props}
         width={110}
         height={50}
