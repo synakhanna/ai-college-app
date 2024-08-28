71a5937020f7dd39c221804a72a4e84cb5d1fd3c
synakhanna <sk9921@nyu.edu>
2024-08-27 20:51:20 -0700

    deleted visual features

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,11 @@ import Features from "@/components/ui/Features";
 import Hero from "@/components/ui/Hero";
 import Pricing from "@/components/ui/Pricing";
 import Testimonial from "@/components/ui/Testimonial";
-import VisualFeatures from "@/components/ui/VisualFeatures";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <VisualFeatures />
       <Features />
       <CTA />
       <Testimonial />
