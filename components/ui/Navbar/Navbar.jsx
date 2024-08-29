1a4656da1979b86dcae20364a4ad5bbcf70d579f
synakhanna <sk9921@nyu.edu>
2024-08-28 23:52:54 -0700

    fixed navbar

--- components/ui/Navbar/Navbar.jsx
+++ components/ui/Navbar/Navbar.jsx
@@ -1,14 +1,14 @@
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Link from 'next/link';
 import { useEffect, useRef, useState } from 'react';
-import { useRouter } from 'next/router'; // Import useRouter
+import { useRouter } from 'next/router'; 
 import NavHeader from '../NavHeader';
 import NavLink from '../NavLink';
 
 const Navbar = () => {
     const [state, setState] = useState(false);
     const menuBtnEl = useRef(null);
-    const router = useRouter(); // Initialize router
+    const router = useRouter();
 
     const soNavigation = [
         { name: "Features", href: "#features" },
@@ -19,19 +19,10 @@ const Navbar = () => {
 
     const userNavigation = [
         { name: "Profile", href: "/profile" },
-        { name: "College", href: "/college" },
+        { name: "Account", href: "/account" }, // Updated from 'College'
         { name: "Counselor", href: "/counselor" },
         { name: "Billing", href: "/billing" },
     ];
-
-    useEffect(() => {
-        document.onclick = (e) => {
-            const target = e.target;
-            if (menuBtnEl.current && !menuBtnEl.current.contains(target)) {
-                setState(false);
-            }
-        };
-    }, []);
 
     const smoothScrollTo = (targetElement) => {
         if (!targetElement) {
@@ -42,7 +33,7 @@ const Navbar = () => {
         const targetPosition = targetElement.getBoundingClientRect().top + window.pageYOffset;
         const startPosition = window.pageYOffset;
         const distance = targetPosition - startPosition;
-        const duration = 2000; // Duration in milliseconds
+        const duration = 2000;
         let startTime = null;
 
         const animation = (currentTime) => {
@@ -66,9 +57,7 @@ const Navbar = () => {
     const handleNavClick = (e, itemHref) => {
         e.preventDefault();
         if (router.pathname !== "/") {
-            // If not on the landing page, navigate to the landing page first
             router.push(`/${itemHref}`).then(() => {
-                // Use setTimeout to give time for the page to load and scroll
                 setTimeout(() => {
                     smoothScrollTo(document.querySelector(itemHref));
                 }, 100);
@@ -78,34 +67,37 @@ const Navbar = () => {
         }
     };
 
+    const toggleMenu = () => {
+        setState((prev) => !prev);
+    };
+
     return (
         <header className="w-full">
             <nav className={`pb-5 md:text-lg md:static md:block bg-gray-900 w-full`}>
                 <div className="custom-screen items-center md:flex max-w-7xl mx-auto w-full">
-                    <NavHeader state={state} onClick={() => setState(!state)} />
+                    <NavHeader state={state} onClick={toggleMenu} />
                     <div className={`flex-1 items-center mt-8 text-gray-300 md:font-medium md:mt-0 md:flex ${state ? 'block' : 'hidden'} md:block`}>
-                        {/* Regular menu */}
                         <SignedOut>
-                        <ul className="hidden lg:flex flex-1 justify-center items-center mt-5 space-y-6 lg:space-x-6 xl:space-x-8 lg:space-y-0">
-                            {soNavigation.map((item, idx) => (
-                                <li key={idx} className="hover:text-gray-50 text-lg font-semibold text-white">
-                                    <Link href={item.href} scroll={false} onClick={(e) => handleNavClick(e, item.href)}>
-                                        {item.name}
-                                    </Link>
-                                </li>
-                            ))}
-                        </ul>
+                            <ul className="hidden lg:flex flex-1 justify-center items-center mt-5 space-y-6 lg:space-x-6 xl:space-x-8 lg:space-y-0">
+                                {soNavigation.map((item, idx) => (
+                                    <li key={idx} className="hover:text-gray-50 text-lg font-semibold text-white">
+                                        <Link href={item.href} scroll={false} onClick={(e) => handleNavClick(e, item.href)}>
+                                            {item.name}
+                                        </Link>
+                                    </li>
+                                ))}
+                            </ul>
                         </SignedOut>
                         <SignedIn>
-                        <ul className="hidden lg:flex flex-1 justify-center items-center mt-5 space-y-6 lg:space-x-6 xl:space-x-8 lg:space-y-0">
-                            {userNavigation.map((item, idx) => (
-                                <li key={idx} className="hover:text-gray-50 text-lg font-semibold text-white">
-                                    <Link href={item.href}>
-                                        {item.name}
-                                    </Link>
-                                </li>
-                            ))}
-                        </ul>
+                            <ul className="hidden lg:flex flex-1 justify-center items-center mt-5 space-y-6 lg:space-x-6 xl:space-x-8 lg:space-y-0">
+                                {userNavigation.map((item, idx) => (
+                                    <li key={idx} className="hover:text-gray-50 text-lg font-semibold text-white">
+                                        <Link href={item.href}>
+                                            {item.name}
+                                        </Link>
+                                    </li>
+                                ))}
+                            </ul>
                         </SignedIn>
                         <div className="hidden lg:flex items-center gap-x-4 pt-5">
                             <SignedOut>
@@ -117,44 +109,40 @@ const Navbar = () => {
                                 </NavLink>
                             </SignedOut>
                             <SignedIn>
-                                <UserButton afterSignOutUrl="/"/>
+                                <UserButton afterSignOutUrl="/" />
                             </SignedIn> 
                         </div>
-                        {/* Hamburger menu button */}
                         <button
                             ref={menuBtnEl}
                             className="lg:hidden ml-auto text-gray-300"
-                            onClick={() => setState(!state)}
+                            onClick={toggleMenu}
                         >
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
-                            </svg>
+                            
                         </button>
                     </div>
                 </div>
-                {/* Hamburger menu items */}
                 <div className={`lg:hidden ${state ? 'block' : 'hidden'} mt-5 bg-gray-900`}>
                     <SignedOut>
-                    <ul className="flex flex-col items-center space-y-6 text-white">
-                        {soNavigation.map((item, idx) => (
-                            <li key={idx} className="hover:text-gray-50 text-lg font-semibold">
-                                <Link href={item.href} scroll={false} onClick={(e) => handleNavClick(e, item.href)}>
-                                    {item.name}
-                                </Link>
-                            </li>
-                        ))}
-                    </ul>
+                        <ul className="flex flex-col items-center space-y-6 text-white">
+                            {soNavigation.map((item, idx) => (
+                                <li key={idx} className="hover:text-gray-50 text-lg font-semibold">
+                                    <Link href={item.href} scroll={false} onClick={(e) => handleNavClick(e, item.href)}>
+                                        {item.name}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
                     </SignedOut>
                     <SignedIn>
-                    <ul className="flex flex-col items-center space-y-6 text-white">
-                        {userNavigation.map((item, idx) => (
-                            <li key={idx} className="hover:text-gray-50 text-lg font-semibold">
-                                <Link href={item.href}>
-                                    {item.name}
-                                </Link>
-                            </li>
-                        ))}
-                    </ul>
+                        <ul className="flex flex-col items-center space-y-6 text-white">
+                            {userNavigation.map((item, idx) => (
+                                <li key={idx} className="hover:text-gray-50 text-lg font-semibold">
+                                    <Link href={item.href}>
+                                        {item.name}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
                     </SignedIn>
                     <div className="flex flex-col items-center space-y-4 mt-6">
                         <SignedOut>
