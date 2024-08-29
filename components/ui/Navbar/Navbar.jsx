ab335d3da0231fe07e5ba945e8dc897feac37286
synakhanna <sk9921@nyu.edu>
2024-08-28 17:52:18 -0700

    fixed hamburger menu

--- components/ui/Navbar/Navbar.jsx
+++ components/ui/Navbar/Navbar.jsx
@@ -85,12 +85,7 @@ const Navbar = () => {
                                 viewBox="0 0 24 24"
                                 xmlns="http://www.w3.org/2000/svg"
                             >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="M4 6h16M4 12h16M4 18h16"
-                                ></path>
+                               
                             </svg>
                         </button>
                     </div>
