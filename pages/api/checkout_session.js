11a09a3a05ebfc55081bae3131a59883a9927d70
Tahmidur_Rabb <tr1476@nyu.edu>
2024-08-29 21:08:52 -0400

    Mongodb

--- pages/api/checkout_session.js
+++ pages/api/checkout_session.js
@@ -9,46 +9,46 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
     apiVersion: "2022-11-15",
 });
 export default async function handler(req, res) {
-    if (req.method === 'POST') {
-      try {
-        const referer = req.headers.referer; // Accessing the Referer header directly
-  
-        const params = {
-          mode: 'subscription',
-          payment_method_types: ['card'],
-          line_items: [
-            {
-              price_data: {
-                currency: 'usd',
-                product_data: {
-                  name: 'Pro subscription',
-                },
-                unit_amount: formatAmountForStripe(10, 'usd'), // $10.00
-                recurring: {
-                  interval: 'month',
-                  interval_count: 1,
-                },
+  if (req.method === 'POST') {
+    try {
+      const referer = req.headers.referer; // Accessing the Referer header directly
+
+      const params = {
+        mode: 'subscription',
+        payment_method_types: ['card'],
+        line_items: [
+          {
+            price_data: {
+              currency: 'usd',
+              product_data: {
+                name: 'Pro subscription',
               },
-              quantity: 1,
+              unit_amount: formatAmountForStripe(10, 'usd'), // $10.00
+              recurring: {
+                interval: 'month',
+                interval_count: 1,
+              },
             },
-          ],
-          success_url: `${referer}result?session_id={CHECKOUT_SESSION_ID}`,
-          cancel_url: `${referer}result?session_id={CHECKOUT_SESSION_ID}`,
-        };
-  
-        const checkoutSession = await stripe.checkout.sessions.create(params);
-  
-        return res.status(200).json(checkoutSession);
-      } catch (error) {
-        console.error('Error creating checkout session:', error);
-        return res.status(500).json({ error: { message: error.message } });
-      }
-    } else {
-      // Handle other HTTP methods or return 405 if method is not allowed
-      res.setHeader('Allow', ['POST']);
-      res.status(405).end(`Method ${req.method} Not Allowed`);
+            quantity: 1,
+          },
+        ],
+        success_url: `${referer}result?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${referer}result?session_id={CHECKOUT_SESSION_ID}`,
+      };
+
+      const checkoutSession = await stripe.checkout.sessions.create(params);
+
+      return res.status(200).json(checkoutSession);
+    } catch (error) {
+      console.error('Error creating checkout session:', error);
+      return res.status(500).json({ error: { message: error.message } });
     }
+  } else {
+    // Handle other HTTP methods or return 405 if method is not allowed
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
   }
+}
 
   export async function GET(req) {
     const searchParams = req.nextUrl.searchParams
