e27f9f326a9206a7271ccb6d93cf6237691e2cbe
Fatima Riaz <fatima_gatech@outlook.com>
2024-09-24 16:28:56 -0500

    Fix for the issue of city search api

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
+
+// module.exports = nextConfig
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*', // Allows requests from any origin
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
