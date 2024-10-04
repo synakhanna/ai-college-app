72c273133582c30911ccce0cfd2ecd0c25249463
Fatima Riaz <fatima_gatech@outlook.com>
2024-10-04 00:57:46 -0500

    reversing the change

--- next.config.js
+++ next.config.js
@@ -1,26 +1,6 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// }
-
-// module.exports = nextConfig
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*', // Allows requests from any origin
-          },
-        ],
-      },
-    ];
-  },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
